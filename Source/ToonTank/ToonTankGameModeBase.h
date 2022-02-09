fb4547ee8831650952da68c185df8a87c9fc60c2
joel <j.settbrg@gmail.com>
2022-02-09 08:05:48 +0100

    initial commit

---
+++ Source/ToonTank/ToonTankGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ToonTankGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOONTANK_API AToonTankGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
